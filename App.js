0916a69712aaa01d588ad82af20a0f9cf3e39185
Regina Mc Guire <rmcguire@antelecom.net>
2019-11-20 09:46:32 -0800

    Initial boilerplate app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Welcome to Location Task App')
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
