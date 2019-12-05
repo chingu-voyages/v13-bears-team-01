3bccee182820fccbc6b3046cb4eb905b7e1191d8
Regina Mc Guire <rmcguire@antelecom.net>
2019-12-04 17:44:10 -0800

    Add date and time pickers to Reminder Form

--- src/components/reminder-form/ReminderForm.js
+++ src/components/reminder-form/ReminderForm.js
@@ -1,60 +1,72 @@
 import React, { useState } from 'react'
-import { Button, Form, Grid } from 'semantic-ui-react'
-
+import { Button, Form, Grid, Segment } from 'semantic-ui-react'
+import DatePicker from 'react-date-picker'
+import TimePicker from 'react-time-picker'
 const ReminderForm = (props) => {
   const [text, setText] = useState('')
-  const [dateTime, setDateTime] = useState('')
+  const [date, setDate] = useState(null)
+  const [time, setTime] = useState(null)
   const [location, setLocation] = useState('')
   const [list, setList] = useState('')
   const [isCompleted, setIsCompleted] = useState(false)
 
   const addReminder = (e) => {
     e.preventDefault()
-    props.onSubmit({
-      text, dateTime, location, list, isCompleted
-    })
+    setIsCompleted(false)
+    props.onSubmit({ text, date, time, location, list, isCompleted })
     setText('')
-    setDateTime('')
+    setDate(null)
+    setTime(null)
     setLocation('')
     setList('')
-    setIsCompleted(false)
   }
 
   return (
     <Grid centered columns={1}>
       <Grid.Column>
         <Form>
-          <Form.Input
-            fluid
-            placeholder='Remind me to...'
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-          <Form.Input
-            fluid
-            icon='clock outline'
-            iconPosition='left'
-            placeholder='On a Day and Time'
-            value={dateTime}
-            onChange={(e) => setDateTime(e.target.value)}
-          />
-          <Form.Input
-            fluid
-            icon='location arrow'
-            iconPosition='left'
-            placeholder='At a Location'
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <Form.Input
-            fluid
-            icon='flag'
-            iconPosition='left'
-            placeholder='Add to a List'
-            value={list}
-            onChange={(e) => setList(e.target.value)}
-          />
-          <Button fluid size='medium' onClick={addReminder}>Done</Button>
+          <Segment>
+            <Form.Input
+              className='form-element'
+              fluid
+              placeholder='Remind me to...'
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <Form.Input
+              className='form-element'
+              fluid
+              icon='location arrow'
+              iconPosition='left'
+              placeholder='At a Location'
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <Form.Input
+              className='form-element'
+              fluid
+              icon='flag'
+              iconPosition='left'
+              placeholder='Add to a List'
+              value={list}
+              onChange={(e) => setList(e.target.value)}
+            />
+            <Segment>
+              <DatePicker
+                className='form-element'
+                value={date}
+                onChange={setDate}
+              />
+            </Segment>
+            <Segment>
+              <TimePicker
+                className='form-element'
+                value={time}
+                onChange={setTime}
+              />
+            </Segment>
+          </Segment>
+          <Button className='button' fluid size='medium' onClick={addReminder}>Done</Button>
         </Form>
       </Grid.Column>
     </Grid>
--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Container, Header, List } from 'semantic-ui-react';
+import React from 'react'
+import { Container, Header, List } from 'semantic-ui-react'
 
-import './home-page.styles.css';
+import './home-page.styles.css'
 
 const HomePage = () => (
-  <div id="home-page">
+  <div id='home-page'>
     <Container text>
-      <section id="reminders">
-        <Header as="h2">Reminders</Header>
+      <section id='reminders'>
+        <Header as='h2'>Reminders</Header>
         <List celled>
           <List.Item>Reminder 1</List.Item>
           <List.Item>Reminder 2</List.Item>
         </List>
       </section>
-      <section id="lists">
-        <Header as="h2">Lists</Header>
+      <section id='lists'>
+        <Header as='h2'>Lists</Header>
         <List celled>
           <List.Item>List 1</List.Item>
           <List.Item>List 2</List.Item>
@@ -22,6 +22,6 @@ const HomePage = () => (
       </section>
     </Container>
   </div>
-);
+)
 
-export default HomePage;
+export default HomePage
