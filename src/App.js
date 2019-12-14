3b4e5c72a5e8242bae6a359aae5768d93a244884
Regina Mc Guire <rmcguire@antelecom.net>
2019-12-13 19:03:10 -0800

    Add hooks to App.js and CreateReminder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/home-page/HomePage'
 import CreateReminder from './pages/create-reminder-page/CreateReminder'
 import EditReminder from './pages/edit-reminder-page/EditReminder'
-const App = () => (
-  <Router>
-    <Switch>
-      <Route path='/' exact>
-        <HomePage />
-      </Route>
-      <Route path='/create'>
-        <CreateReminder />
-      </Route>
-      <Route path='/edit/:id'>
-        <EditReminder />
-      </Route>
-    </Switch>
-  </Router>
-)
-
+import testReminders from './tests/fixtures/reminders'
+const App = () => {
+  const [reminders, setReminders] = useState([])
+  useEffect(() => {
+    setReminders(testReminders)
+  }, [])
+  console.log('app changed', reminders)
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/create'>
+          <CreateReminder reminders={reminders} setReminders={setReminders} />
+        </Route>
+        <Route path='/edit/:id'>
+          <EditReminder />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
 export default App
--- src/pages/create-reminder-page/CreateReminder.js
+++ src/pages/create-reminder-page/CreateReminder.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ReminderForm from '../../components/reminder-form/ReminderForm'
 import { Container, Header } from 'semantic-ui-react'
 
-const CreateReminder = () => {
+const CreateReminder = (props) => {
   const onSubmit = (reminder) => {
-    console.log(reminder)
+    props.setReminders(props.reminders.concat(reminder))
+    console.log('create new reminder', props.reminders)
   }
   return (
     <Container>
